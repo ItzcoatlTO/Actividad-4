baff3bb70377435e437f3f94fe7969d2766681ae
Itzcoatl Torres Ortiz <AL03004004@tecmilenio.mx>
2024-02-16 18:27:48 -0600

    Actividad 4

---
+++ Actividad 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividad 4
+//
+//  Created by Itzcoatl on 16/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
